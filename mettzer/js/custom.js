b37510157ffe45164ffd5bf651d037b4c04edd8f
lleos <leonardosalvador81@gmail.com>
2016-11-30 21:51:38 -0200

    ajustments in the html css

--- mettzer/js/custom.js
+++ mettzer/js/custom.js
@@ -45,6 +45,22 @@ $(function() {
   });
 });
 
+$(function() { var logo = $(".lrg-logo"); $(window).scroll(function() {
+var scroll = $(window).scrollTop();
+    if (scroll >= 90) {
+      if(!logo.hasClass("sml-logo")) {
+        logo.hide();
+        logo.removeClass('lrg-logo').addClass("sml-logo").fadeIn( "slow");
+      }
+    } else {
+      if(!logo.hasClass("lrg-logo")) {
+        logo.hide();
+        logo.removeClass("sml-logo").addClass('lrg-logo').fadeIn( "slow");
+      }
+    }
+  });
+});
+
 /*** Wow plugin bottom offset calculation*/
 $(".wow").each(function() {
   var wowHeight = $(this).height();
